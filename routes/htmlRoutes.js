d285b044a3c080875b50705da02f7f7bba96162b
Joshua DiFrancesco <joshiqua@gmail.com>
2022-01-14 19:36:45 -0500

    added html routes to /notes and all

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,21 +2,12 @@ const path = require('path');
 
 const router = require('express').Router();
 
-/*Send Notes Route:
-The first route uses the sendFile() method of res object which send notes.html file
-from the public folder if there is a request going through this route.
-*/
-
+//Sends notes.html if there is a request
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
-/*Send Homepage Route:
-This second route is for all other requests, requests with no file extension.  It sends users back to
-the homepage, the index.html in the public folder.  It does this instead of sending nothing at all because there might be 
-some errors in routing.  
-*/
-
+//Used for all other routes (aside from the /notes)
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
